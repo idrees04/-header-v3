8e0e11d592c283a966514d4d9f1ab57b1bcbbff9
Muhammad Idrees <midrees0045@gmail.com>
2026-04-29 08:33:28 +0500

    fix button import

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -5,8 +5,7 @@ import { PanelLeft } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+
 import { Separator } from "@/components/ui/separator"
 import {
   Sheet,
@@ -22,6 +21,8 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip"
+import { Button } from "./Button"
+import { Input } from "./Input"
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
